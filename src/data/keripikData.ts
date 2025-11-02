e731815de3aa6d719903171a2f835af0c99fe036
ntshap <natashaputrir@students.amikom.ac.id>
2025-11-03 02:53:35 +0700

    feat: Expand Keripik category from 20 to 60 products with complete data

--- src/data/keripikData.ts
+++ src/data/keripikData.ts
@@ -1,6 +1,6 @@
 /**
  * Data Produk Keripik - Toko Lezat
- * File ini berisi data lengkap untuk 20 produk kategori Keripik
+ * File ini berisi data lengkap untuk 60 produk kategori Keripik
  */
 
 // Import gambar produk Keripik
@@ -24,6 +24,45 @@ import keripikPare from "@/assets/image/KERIPIK/KERIPIK PARE.jpg";
 import lantingBawangSuper from "@/assets/image/KERIPIK/LANTING BAWANG SUPER.jpg";
 import potelTumbarUnyil from "@/assets/image/KERIPIK/POTEL TUMBAR UNYIL.jpg";
 import lantingManisPedas from "@/assets/image/KERIPIK/LANTING MANIS PEDAS.jpg";
+import potelTumbar from "@/assets/image/KERIPIK/POTEL TUMBAR.jpg";
+import lantingMpMini from "@/assets/image/KERIPIK/LANTING MP MINI.jpg";
+import lantingUdangPedas from "@/assets/image/KERIPIK/LANTING UDANG PEDAS.jpg";
+import talesStik from "@/assets/image/KERIPIK/TALES STIK.jpg";
+import mixNut from "@/assets/image/KERIPIK/MIX NUT.jpg";
+import balokPedas from "@/assets/image/KERIPIK/BALOK PEDAS.jpg";
+import balokAsin from "@/assets/image/KERIPIK/BALOK ASIN.jpg";
+import makaroniBantatPedas from "@/assets/image/KERIPIK/MAKARONI BANTAT PEDAS.jpg";
+import bremDavronas from "@/assets/image/KERIPIK/BREM DAVRONAS.jpg";
+import singkongKeju from "@/assets/image/KERIPIK/SINGKONG KEJU.jpg";
+import twistPedas from "@/assets/image/KERIPIK/TWIST PEDAS.jpg";
+import potatoMiniKeju from "@/assets/image/KERIPIK/POTATO MINI KEJU.jpg";
+import potatoSaos from "@/assets/image/KERIPIK/POTATO SAOS.jpg";
+import makaroniAsin from "@/assets/image/KERIPIK/MAKARONI ASIN.jpg";
+import makaroniRujukCincin from "@/assets/image/KERIPIK/MAKARONI RUJAK CINCIN.jpg";
+import rambakBalado from "@/assets/image/KERIPIK/RAMBAK BALADO.jpg";
+import rambakBulat from "@/assets/image/KERIPIK/RAMBAK BULAT.jpg";
+import rengginanBawang from "@/assets/image/KERIPIK/RENGGINAN BAWANG.jpg";
+import rengginanTerasi from "@/assets/image/KERIPIK/RENGGINAN TERASI.jpg";
+import singkongBakarBalado from "@/assets/image/KERIPIK/SINGKONG BAKAR BALADO.jpg";
+import tengiriBulat from "@/assets/image/KERIPIK/TENGIRI BULAT.jpg";
+import tengiriPenceng from "@/assets/image/KERIPIK/TENGIRI PENCENG.jpg";
+import cipirIkan from "@/assets/image/KERIPIK/CIPIR IKAN.jpg";
+import opakPedas from "@/assets/image/KERIPIK/OPAK PEDAS.jpg";
+import slondokKriboPedas from "@/assets/image/KERIPIK/SLONDOK KRIBO PEDAS.jpg";
+import ceripingGetukHitamSr from "@/assets/image/KERIPIK/CERIPING GETUK HITAM SR.jpg";
+import ceripingGetukKuningManisPedas from "@/assets/image/KERIPIK/CERIPING GETUK KUNING MANIS PEDAS.jpg";
+import ceripingGetukHitam from "@/assets/image/KERIPIK/CERIPING GETUK HITAM.jpg";
+import stikCumiHitam from "@/assets/image/KERIPIK/STIK CUMI HITAM.jpg";
+import slondokUdang from "@/assets/image/KERIPIK/SLONDOK UDANG.jpg";
+import cakarAyamTop from "@/assets/image/KERIPIK/CAKAR AYAM TOP.jpg";
+import singkongOrlinExtraHot from "@/assets/image/KERIPIK/SINGKONG ORLIN EXTRA HOT.jpg";
+import slondokKeriting from "@/assets/image/KERIPIK/SLONDOK KERITING.jpg";
+import kacangAtomKroma from "@/assets/image/KERIPIK/KACANG ATOM KROMA.jpg";
+import pillowMiniCoklat from "@/assets/image/KERIPIK/PILLOW MINI COKLAT.jpg";
+import ceripingGadung from "@/assets/image/KERIPIK/CERIPING GADUNG.jpg";
+import kerupukKoinUdang from "@/assets/image/KERIPIK/KERUPUK KOIN UDANG.jpg";
+import sukuBulat from "@/assets/image/KERIPIK/SUKUN BULAT.jpg";
+import ususAyamSuper from "@/assets/image/KERIPIK/USUS AYAM SUPER.jpg";
 
 export interface KeripikProduct {
   id: number;
@@ -215,6 +254,367 @@ export const keripikProducts: KeripikProduct[] = [
     description: "Lanting manis pedas dengan perpaduan rasa manis dan pedas yang pas",
     category: "Keripik",
     weight: "240 gr"
+  },
+  // Produk tambahan (3021-3060)
+  {
+    id: 3021,
+    name: "POTEL TUMBAR",
+    price: 13500,
+    image: potelTumbar,
+    description: "Potel tumbar dengan rasa rempah tumbar yang gurih dan lezat",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3022,
+    name: "LANTING MP MINI",
+    price: 11000,
+    image: lantingMpMini,
+    description: "Lanting MP mini dengan rasa gurih yang pas untuk camilan",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3023,
+    name: "LANTING UDANG PEDAS",
+    price: 13500,
+    image: lantingUdangPedas,
+    description: "Lanting udang pedas dengan kombinasi rasa udang dan cabai",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3024,
+    name: "TALES STIK",
+    price: 15500,
+    image: talesStik,
+    description: "Keripik tales stik dengan tekstur renyah dan rasa manis gurih",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3025,
+    name: "MIX NUT",
+    price: 19000,
+    image: mixNut,
+    description: "Keripik mix nut dengan kombinasi kacang-kacangan yang bergizi",
+    category: "Keripik",
+    weight: "475 gr"
+  },
+  {
+    id: 3026,
+    name: "BALOK PEDAS",
+    price: 14000,
+    image: balokPedas,
+    description: "Keripik balok pedas dengan rasa pedas yang menggigit",
+    category: "Keripik",
+    weight: "490 gr"
+  },
+  {
+    id: 3027,
+    name: "BALOK ASIN",
+    price: 14000,
+    image: balokAsin,
+    description: "Keripik balok asin dengan tekstur renyah dan rasa asin gurih",
+    category: "Keripik",
+    weight: "490 gr"
+  },
+  {
+    id: 3028,
+    name: "MAKARONI BANTAT PEDAS",
+    price: 12000,
+    image: makaroniBantatPedas,
+    description: "Keripik makaroni bantat dengan bumbu pedas yang menggugah selera",
+    category: "Keripik",
+    weight: "290 gr"
+  },
+  {
+    id: 3029,
+    name: "BREM DAVRONAS",
+    price: 18500,
+    image: bremDavronas,
+    description: "Keripik brem davronas dengan rasa tradisional yang autentik",
+    category: "Keripik",
+    weight: "33 biji"
+  },
+  {
+    id: 3030,
+    name: "SINGKONG KEJU",
+    price: 17500,
+    image: singkongKeju,
+    description: "Keripik singkong keju dengan rasa keju yang gurih dan lezat",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3031,
+    name: "TWIST PEDAS",
+    price: 14500,
+    image: twistPedas,
+    description: "Keripik twist pedas dengan bentuk unik dan rasa pedas menggugah",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3032,
+    name: "POTATO MINI KEJU",
+    price: 15500,
+    image: potatoMiniKeju,
+    description: "Keripik potato mini keju dengan rasa keju yang creamy",
+    category: "Keripik",
+    weight: "230 gr"
+  },
+  {
+    id: 3033,
+    name: "SUPER ROLL PANDAN",
+    price: 9000,
+    image: potatoMiniKeju,
+    description: "Keripik super roll pandan dengan aroma pandan yang harum",
+    category: "Keripik",
+    weight: "185 gr"
+  },
+  {
+    id: 3034,
+    name: "POTATO SAOS",
+    price: 12750,
+    image: potatoSaos,
+    description: "Keripik potato saos dengan rasa saos yang lezat dan gurih",
+    category: "Keripik",
+    weight: "180 gr"
+  },
+  {
+    id: 3035,
+    name: "MAKARONI ASIN",
+    price: 11500,
+    image: makaroniAsin,
+    description: "Keripik makaroni asin dengan tekstur renyah dan rasa gurih",
+    category: "Keripik",
+    weight: "235 gr"
+  },
+  {
+    id: 3036,
+    name: "MAKARONI RUJAK CINCIN",
+    price: 13000,
+    image: makaroniRujukCincin,
+    description: "Keripik makaroni rujak cincin dengan rasa rujak yang khas",
+    category: "Keripik",
+    weight: "235 gr"
+  },
+  {
+    id: 3037,
+    name: "RAMBAK BALADO",
+    price: 19500,
+    image: rambakBalado,
+    description: "Keripik rambak balado dengan bumbu balado yang pedas dan gurih",
+    category: "Keripik",
+    weight: "80 gr"
+  },
+  {
+    id: 3038,
+    name: "RAMBAK BULAT",
+    price: 21000,
+    image: rambakBulat,
+    description: "Keripik rambak bulat dengan bentuk bulat yang menarik",
+    category: "Keripik",
+    weight: "80 gr"
+  },
+  {
+    id: 3039,
+    name: "RENGGINAN BAWANG",
+    price: 18500,
+    image: rengginanBawang,
+    description: "Keripik rengginan bawang dengan aroma bawang yang harum",
+    category: "Keripik",
+    weight: "235 gr"
+  },
+  {
+    id: 3040,
+    name: "RENGGINAN TERASI",
+    price: 23000,
+    image: rengginanTerasi,
+    description: "Keripik rengginan terasi dengan rasa terasi yang kuat dan gurih",
+    category: "Keripik",
+    weight: "285 gr"
+  },
+  {
+    id: 3041,
+    name: "SINGKONG BAKAR BALADO",
+    price: 16500,
+    image: singkongBakarBalado,
+    description: "Keripik singkong bakar balado dengan rasa bakar dan pedas",
+    category: "Keripik",
+    weight: "240 gr"
+  },
+  {
+    id: 3042,
+    name: "TENGIRI BULAT",
+    price: 27000,
+    image: tengiriBulat,
+    description: "Keripik tengiri bulat dengan rasa ikan tengiri yang gurih",
+    category: "Keripik",
+    weight: "180 gr"
+  },
+  {
+    id: 3043,
+    name: "TENGIRI PENCENG",
+    price: 27000,
+    image: tengiriPenceng,
+    description: "Keripik tengiri penceng dengan bentuk miring yang unik",
+    category: "Keripik",
+    weight: "180 gr"
+  },
+  {
+    id: 3044,
+    name: "CIPIR IKAN",
+    price: 12500,
+    image: cipirIkan,
+    description: "Keripik cipir ikan dengan tekstur renyah dan gurih",
+    category: "Keripik",
+    weight: "205 gr"
+  },
+  {
+    id: 3045,
+    name: "OPAK PEDAS",
+    price: 15500,
+    image: opakPedas,
+    description: "Keripik opak pedas dengan rasa pedas yang menggugah",
+    category: "Keripik",
+    weight: "230 gr"
+  },
+  {
+    id: 3046,
+    name: "SLONDOK KRIBO PEDAS",
+    price: 14000,
+    image: slondokKriboPedas,
+    description: "Keripik slondok kribo pedas dengan tekstur kribo yang renyah",
+    category: "Keripik",
+    weight: "280 gr"
+  },
+  {
+    id: 3047,
+    name: "CERIPING GETUK HITAM SR",
+    price: 22500,
+    image: ceripingGetukHitamSr,
+    description: "Keripik ceriping getuk hitam special dengan rasa getuk tradisional",
+    category: "Keripik",
+    weight: "260 gr"
+  },
+  {
+    id: 3048,
+    name: "CERIPING GETUK KUNING MANIS PEDAS",
+    price: 16000,
+    image: ceripingGetukKuningManisPedas,
+    description: "Keripik ceriping getuk kuning dengan rasa manis pedas",
+    category: "Keripik",
+    weight: "330 gr"
+  },
+  {
+    id: 3049,
+    name: "CERIPING GETUK HITAM",
+    price: 17500,
+    image: ceripingGetukHitam,
+    description: "Keripik ceriping getuk hitam dengan warna dan rasa khas",
+    category: "Keripik",
+    weight: "330 gr"
+  },
+  {
+    id: 3050,
+    name: "STIK CUMI HITAM",
+    price: 15000,
+    image: stikCumiHitam,
+    description: "Keripik stik cumi hitam dengan rasa cumi yang gurih",
+    category: "Keripik",
+    weight: "180 gr"
+  },
+  {
+    id: 3051,
+    name: "SLONDOK UDANG",
+    price: 19000,
+    image: slondokUdang,
+    description: "Keripik slondok udang dengan rasa udang yang lezat",
+    category: "Keripik",
+    weight: "230 gr"
+  },
+  {
+    id: 3052,
+    name: "CAKAR AYAM TOP",
+    price: 42500,
+    image: cakarAyamTop,
+    description: "Keripik cakar ayam top dengan tekstur renyah dan gurih",
+    category: "Keripik",
+    weight: "185 gr"
+  },
+  {
+    id: 3053,
+    name: "SINGKONG ORLIN EXTRA HOT",
+    price: 13000,
+    image: singkongOrlinExtraHot,
+    description: "Keripik singkong orlin extra hot dengan rasa sangat pedas",
+    category: "Keripik",
+    weight: "150 gr"
+  },
+  {
+    id: 3054,
+    name: "SLONDOK KERITING",
+    price: 19000,
+    image: slondokKeriting,
+    description: "Keripik slondok keriting dengan tekstur keriting yang renyah",
+    category: "Keripik",
+    weight: "280 gr"
+  },
+  {
+    id: 3055,
+    name: "KACANG ATOM KROMA",
+    price: 18000,
+    image: kacangAtomKroma,
+    description: "Keripik kacang atom kroma dengan rasa gurih dan gurih",
+    category: "Keripik",
+    weight: "340 gr"
+  },
+  {
+    id: 3056,
+    name: "PILLOW MINI COKLAT",
+    price: 13500,
+    image: pillowMiniCoklat,
+    description: "Keripik pillow mini coklat dengan rasa coklat yang manis",
+    category: "Keripik",
+    weight: "290 gr"
+  },
+  {
+    id: 3057,
+    name: "CERIPING GADUNG",
+    price: 21000,
+    image: ceripingGadung,
+    description: "Keripik ceriping gadung dengan tekstur renyah dan tradisional",
+    category: "Keripik",
+    weight: "280 gr"
+  },
+  {
+    id: 3058,
+    name: "KERUPUK KOIN UDANG",
+    price: 16000,
+    image: kerupukKoinUdang,
+    description: "Keripik kerupuk koin udang dengan bentuk koin yang menarik",
+    category: "Keripik",
+    weight: "180 gr"
+  },
+  {
+    id: 3059,
+    name: "SUKUN BULAT",
+    price: 20000,
+    image: sukuBulat,
+    description: "Keripik sukun bulat dengan bentuk bulat dan rasa gurih",
+    category: "Keripik",
+    weight: "280 gr"
+  },
+  {
+    id: 3060,
+    name: "USUS AYAM SUPER",
+    price: 45000,
+    image: ususAyamSuper,
+    description: "Keripik usus ayam super dengan tekstur super renyah dan gurih",
+    category: "Keripik",
+    weight: "235 gr"
   }
 ];
 
